64fe7a9e043870e14e44322cbbc47b93e1698e2d
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-10-26 02:44:46 +0200

    añadida url al bulbasaur :(

--- TEMA 2 - DOM/EJERCICIOS/II DOM/Ej6/main.js
+++ TEMA 2 - DOM/EJERCICIOS/II DOM/Ej6/main.js
@@ -1,9 +1,9 @@
 function evoluciona(){
     var imagen = document.getElementById("evolucion");
-    if (imagen.src === "http://localhost:63343/DWEC23-24/TEMA%202%20-%20DOM/EJERCICIOS/II%20DOM/Ej6/resources/bulbasaur.jpg"){
+    if (imagen.src === "https://dalonsolaz.asyncore.es/DWEC23-24/TEMA%202%20-%20DOM/EJERCICIOS/II%20DOM/Ej6/resources/bulbasaur.jpg"){
         imagen.src = "resources/ivysaur.jpg";
         imagen.alt = "Ivysaur";
-    }else if (imagen.src === "http://localhost:63343/DWEC23-24/TEMA%202%20-%20DOM/EJERCICIOS/II%20DOM/Ej6/resources/ivysaur.jpg"){
+    }else if (imagen.src === "https://dalonsolaz.asyncore.es/DWEC23-24/TEMA%202%20-%20DOM/EJERCICIOS/II%20DOM/Ej6/resources/ivysaur.jpg"){
         imagen.src = "resources/venusaur.jpg";
         imagen.alt = "Venusaur";
     }else{
